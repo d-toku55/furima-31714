a93468689d50e4c579cfb2a18330046e1b74b873
d-toku55 <hdd550z4@gmail.com>
2020-11-06 15:57:59 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,18 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :email, inclusion: { in: ["@"] }
+
+  validates_format_of :password, with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i,
+  uniqueness: true, length: { minimum: 6 }, confirmation: true
+  validates :encrypted_password_confirmation, presence: true
+
+  validates_format_of :family_name,         presence: true, with: /\A[ぁ-んァ-ン一-龥]+\z/
+  validates_format_of :first_name,          presence: true, with: /\A[ぁ-んァ-ン一-龥]+\z/
+  validates_format_of :family_name_reading, presence: true, wiih: /\p{katakana}/
+  validates_format_of :first_name_reading,  presence: true, wiih: /\p{katakana}/
+  validates :nickname,                      presence: true
+  validates :birthday,                      presence: true
+
 end
