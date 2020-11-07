15639137475342226c50a4d7e226e8f2bd37cc28
d-toku55 <hdd550z4@gmail.com>
2020-11-07 23:58:06 +0900

    ユーザー管理機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :email, inclusion: { in: ["@"] }
+  has_many :items
+  has_many :records
 
-  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i },
-  uniqueness: true, length: { minimum: 6 }, confirmation: true
-  validates :encrypted_password_confirmation, presence: true
-
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
+  validates :encrypted_password, presence: true
   validates :family_name,         format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }, presence: true
   validates :first_name,          format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }, presence: true
   validates :family_name_reading, format: { with: /\p{katakana}/ }, presence: true
