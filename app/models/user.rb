6b09372d56913793e6f0955d21962bf3ba0f9165
d-toku55 <hdd550z4@gmail.com>
2020-11-09 11:55:55 +0900

    正規表現を変数に代入

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,12 +7,15 @@ class User < ApplicationRecord
   has_many :items
   has_many :records
 
+  NAME_REGEX = /\A[ぁ-んァ-ン一-龥]+\z/
+  NAME_READING_REGEX = /\p{katakana}/
+
   validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
   validates :encrypted_password, presence: true
-  validates :family_name,         format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }, presence: true
-  validates :first_name,          format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }, presence: true
-  validates :family_name_reading, format: { with: /\p{katakana}/ }, presence: true
-  validates :first_name_reading,  format: { with: /\p{katakana}/ }, presence: true
+  validates :family_name,         format: { with: NAME_REGEX }, presence: true
+  validates :first_name,          format: { with: NAME_REGEX }, presence: true
+  validates :family_name_reading, format: { with: NAME_READING_REGEX }, presence: true
+  validates :first_name_reading,  format: { with: NAME_READING_REGEX }, presence: true
   validates :nickname,                      presence: true
   validates :birthday,                      presence: true
 end
