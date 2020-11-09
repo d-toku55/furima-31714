b9c05bcf447b774d395312e7a97eee76dbec731f
d-toku55 <hdd550z4@gmail.com>
2020-11-09 12:12:52 +0900

    @がないと登録できないテストの追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -100,5 +100,10 @@ RSpec.describe User, type: :model do
       another_user.valid?
       expect(another_user.errors.full_messages).to include('Email has already been taken')
     end
+    it "emailに@がないと、登録できない" do
+      @user.email = 'aaaaa.com'
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Email is invalid")
+    end
   end
 end
