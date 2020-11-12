f2bd3b7274594d8285b9857799b21aae4d14a648
d-toku55 <hdd550z4@gmail.com>
2020-11-12 20:55:35 +0900

    未ログインでも観覧できるように修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :new]
+  before_action :authenticate_user!, except: [:index, :new, :show]
 
   def index
     @items = Item.all.order(created_at: :desc)
