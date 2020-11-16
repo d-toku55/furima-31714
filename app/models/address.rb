3c9d6a335ef797675f8b08a218a0f31f3930b64c
d-toku55 <hdd550z4@gmail.com>
2020-11-16 21:42:20 +0900

    モデルの作成

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,13 +1,8 @@
 class Address < ApplicationRecord
 
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :prefecture
+
   belongs_to :record
 
-  with_options presence: true do
-    validates :postal_code, format: { with: /\A\d{3}[-]\d{4}\z/ }
-    validates :prefectures_id
-    validates :municipality
-    validates :address
-    validates :phone_number, format: { with: less_than_or_equal_to: 11 }
-  end
-  
 end
